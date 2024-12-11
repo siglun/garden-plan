cb2029c1f5628d1bb8c70f817cf2a9287ffb4379
Sigfrid Lundberg <siggelundberg@gmail.com>
2024-12-11 18:00:50 +0100

    principle ok

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-groff -e -p -mpapersize  -dpaper=a4 -P-pa4 \
+groff -e -p -mpapersize  -dpaper=a4 -P-pa4 -tk -Kutf8   \
       -Tps -ms parameters.ms  garden-plan.pic > garden-plan.ps
 
 ps2pdf garden-plan.ps
